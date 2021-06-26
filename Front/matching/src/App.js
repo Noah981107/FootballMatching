de868a1fdff7e28e8352c2470a27573ba6159ec0
Noah981107 <shgustmd6865@naver.com>
2021-06-26 12:53:28 +0900

    Renew Front
    Renew Front

---
+++ Front/matching/src/App.js
@@ -0,0 +1,23 @@
+/*eslint-disable*/
+
+import React, {useState} from 'react';
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="App">
+      <div className = "green-nav">
+        <div className = "title">BaroCha</div>
+        <div className = "buttons">
+          <button className = "login_button">로그인</button>
+          <button className = "signUp_button">회원 가입</button>
+        </div>
+      </div>
+
+    </div>
+  );
+}
+
+
+export default App;
